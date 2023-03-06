a5c1026c671c51e8d3b6c9a375e043bdb9325de9
Krisna Rusdiono <krisnarusdiono@gmail.com>
2023-03-06 14:36:06 +0700

    feat: initi custom directive

---
+++ src/directives/highlight.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(
+    private el: ElementRef
+  ) { }
+
+  @Input() highlightBackground = 'green';
+  
+  @HostListener('mouseenter') onMouseEnter () {
+    this.hightlight(this.highlightBackground);
+  }
+
+  @HostListener('mouseleave') onMouseLeave () {
+    this.hightlight('');
+  }
+
+  hightlight(highlightBackground: string) {
+    this.el.nativeElement.style.backgroundColor = highlightBackground;
+  }
+
+}
