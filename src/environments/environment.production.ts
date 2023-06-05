ebbead83073045faeca459b4cf19eb379edf2406
Krisna Rusdiono <krisnarusdiono@gmail.com>
2023-06-05 14:06:38 +0700

    chore: update development port for avoid cors with new backend base project

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://radyalabs.asuscomm.com:6031/api',
-  };
-  
+  production: true,
+  apiUrl: 'http://radyalabs.asuscomm.com:6031/api',
+};
